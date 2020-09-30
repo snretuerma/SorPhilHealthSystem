0bd18f09a1080f9c6cb9cd134676498ce70e16af
kennael22 <55552649+kennael22@users.noreply.github.com>
2020-09-30 10:26:12 +0800

    Budget sort start date (#10)
    * sort start date
    
    * sort date
    
    * sort date
    
    * fix error in usercontroller line 75
    
    * fix missing function user and admin
    
    Co-authored-by: kennael22 <kennael22@yahoo.com>

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -32,8 +32,21 @@ class AdminController extends Controller
         $budget = DB::table('budgets')
             ->join('hospitals', 'budgets.hospital_id', '=', 'hospitals.id')
             ->select('budgets.*', 'hospitals.hospital_code')
+            ->whereNull('budgets.deleted_at')
+            ->orderby('start_date','desc')
             ->get();
         return $budget;
+    }
+    public function addBudget(Request $request)
+    {
+        $budget = new Budget;
+        $startdate = Carbon::parse($request->start_date)->format('Y-m-d H:i:s');
+        $enddate= Carbon::parse($request->end_date)->format('Y-m-d H:i:s');
+        $budget->start_date=$startdate;
+        $budget->total = $request->total;
+        $budget->end_date=$enddate;
+        $budget->hospital_id=$request->codeholder;
+        $budget->save();
     }
     public function editBudget(Request $request)
     {
