a95713ec94df86b81a267af24fd97628a87dff3b
paulbryandy <70583648+paulbryandy@users.noreply.github.com>
2020-11-23 13:59:30 +0800

    Doctor ui (#42)
    * fixed migration
    
    * remove old component with their functions, etc.
    
    * tag error fixed

--- resources/js/components/user/DoctorComponent.vue
+++ resources/js/components/user/DoctorComponent.vue
@@ -1,16 +1,17 @@
 <template>
     <div>
-         <!-- Header -->
+        <!-- Header -->
         <div class="row header-top">
             <div class="header-title-parent">
                 <span class="header-title">
-                <i class="fa fa-file-medical-alt"></i>&nbsp;&nbsp;Records
+                <i class="fa fa-user-md"></i>&nbsp;&nbsp;Doctors
                 </span>
             </div>
         </div>
         <!-- End Header -->
     </div>
 </template>
+
 <script>
 export default {
 
