19210d64ab933a2c62321da1631a58970a8e690e
kennael22 <johnkevin.noguera@gmail.com>
2020-12-03 16:59:54 +0800

    travis_fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -359,22 +359,24 @@ class UserController extends Controller
     }
     public function getRecords()
     {
-        $records=CreditRecord::with('hospital','doctors')
+        $records=CreditRecord::with('hospital', 'doctors')
         ->get();
         return response()->json($records);
     }
 
-    public function addCreditRecord(addCreditRecordRequest $request)
+    public function addCreditRecord(AddCreditRecordRequest $request)
     {
             $record = new CreditRecord;
             $record->hospital()->associate(Hospital::find(1)->id);
             $record->patient_name = $request->name;
             $record->batch = $request->batch;
-            $record->admission_date = Carbon::parse($request->admission)->setTimezone('Asia/Manila');
-            $record->discharge_date = Carbon::parse($request->discharge)->setTimezone('Asia/Manila');
+            $record->admission_date = Carbon::parse($request->admission)
+            ->setTimezone('Asia/Manila');
+            $record->discharge_date = Carbon::parse($request->discharge)
+            ->setTimezone('Asia/Manila');
             // if public
             // else
-            if($request->admission >= '2020-03-1') {
+            if ($request->admission >= '2020-03-1') {
                 $record->record_type="new";
                 $record->total = $request->pf;
                 $record->non_medical_fee = $request->pf/2;
@@ -388,20 +390,22 @@ class UserController extends Controller
                     ->where('is_parttime', false)
                     ->pluck('id')
                     ->toArray();
-                $part_time_doctors = Doctor::select('id')->where('is_active', true)->where('is_parttime', true)->pluck('id')->toArray();
+                $part_time_doctors = Doctor::select('id')->where('is_active', true)
+                ->where('is_parttime', true)->pluck('id')->toArray();
                 $pooled_record = new PooledRecord;
                 $pooled_record->full_time_doctors = json_encode($full_time_doctors);
                 $pooled_record->part_time_doctors = json_encode($part_time_doctors);
                 $total_pooled_fee = $record->non_medical_fee*0.3;
-                $initial_individual_fee = ($record->non_medical_fee*0.3)/(count($full_time_doctors)+(count($part_time_doctors)/2));
-    
+                $initial_individual_fee = ($record->non_medical_fee*0.3)/
+                (count($full_time_doctors)+
+                (count($part_time_doctors)/2));
                 $pooled_record->full_time_individual_fee = $initial_individual_fee;
                 $pooled_record->part_time_individual_fee = $initial_individual_fee/2;
                 $pooled_record->record_id = $record->id;
                 $pooled_record->save();
-                foreach($doctors as $doctor) {
-                    foreach($request->doctortype as $types_of_doctors){
-                        if($doctor->id == $types_of_doctors['id']) {
+                foreach ($doctors as $doctor) {
+                    foreach ($request->doctortype as $types_of_doctors) {
+                        if ($doctor->id == $types_of_doctors['id']) {
                             $doctor->credit_records()->attach($record->id, [
                                 'doctor_role' => $types_of_doctors['role'],
                                 'professional_fee' => ($record->non_medical_fee*0.7)/$doctor->count()
@@ -426,7 +430,8 @@ class UserController extends Controller
         ->get();
     }
 
-    public function getBatch(){
+    public function getBatch()
+    {
         return CreditRecord::distinct()
         ->where('hospital_id', Auth::user()->hospital_id)
         ->orderBy('batch', 'DESC')
