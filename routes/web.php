4aba28897a01c585bd0e0cf79ab218d60e6b55bc
kennael22 <55552649+kennael22@users.noreply.github.com>
2020-12-07 11:52:05 +0800

    Records (#46)
    * with other physician
    
    * Merge branch 'development' of https://github.com/snretuerma/SorPhilHealthSystem into records
    
    * add_child components
    
    * changes
    
    * Merge branch 'development' of https://github.com/snretuerma/SorPhilHealthSystem into records
    
    * with list of doctors
    
    * doctors
    
    * Merge branch 'development' of https://github.com/snretuerma/SorPhilHealthSystem into records
    
    * with public_private and insert
    
    * Merge branch 'development' of https://github.com/snretuerma/SorPhilHealthSystem into records
    
    * travis_fix
    
    * travis_fix2
    
    * travis_fix3
    
    * insert_record
    
    * remove_space
    
    * uncomment_comment
    
    * remove_space
    
    * css
    
    * white_space,route,comment
    
    * underscore
    
    * css

--- routes/web.php
+++ routes/web.php
@@ -73,12 +73,12 @@ Route::group(['prefix' => 'user', 'middleware' => 'auth'], function () {
 
     //Doctors
     Route::get('/doctors', 'UserController@doctors')->name('doctors');
-    Route::get('get_doctors', 'UserController@getDoctors');
+    // Route::get('get_doctors', 'UserController@getDoctors');
     Route::post('add_doctor', 'UserController@addDoctor');
     Route::put('edit_doctor', 'UserController@editDoctor');
     Route::delete('delete_doctor/{id}', 'UserController@deleteDoctor');
     //Records
-    Route::get('/records', 'UserController@records')->name('records');
+    // Route::get('/records', 'UserController@records')->name('records');
 
     //Restore
     Route::get('/restore', 'UserController@restore')->name('restore');
@@ -98,6 +98,16 @@ Route::group(['prefix' => 'user', 'middleware' => 'auth'], function () {
     //Setting
     Route::get('/setting', 'UserController@setting')->name('setting');
     Route::post('update_setting', 'UserController@updateSetting');
+
+    //doctors
+    Route::get('get_doctors', 'UserController@getDoctors');
+    Route::get('get_active_doctors', 'UserController@getActiveDoctors');
+
+    //user record
+    Route::get('/records', 'UserController@records')->name('records');
+    Route::get('/get_records', 'UserController@getRecords');
+    Route::post('/add_records', 'UserController@addCreditRecord');
+    Route::get('/get_batch', 'UserController@getBatch');
 });
 Route::group(['prefix' => 'observer', 'middleware' => 'auth'], function () {
     Route::get('/', 'ObserverController@index')->name('observer');
