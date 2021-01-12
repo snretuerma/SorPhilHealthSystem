4accb8e0323df37f9f4f059f18a34361d9b8aa3d
MarkDy2797 <markdy61@gmail.com>
2021-01-12 15:07:38 +0800

    add start bat file

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 start php -S localhost:8000 -t public
-start php artisan queue:work --tries=5
+@REM start php artisan queue:work --tries=5
 start php artisan queue:listen
 start "" "chrome" --kiosk --fullscreen "http://localhost:8000"
